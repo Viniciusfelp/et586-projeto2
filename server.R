935233ff03f823aab149341be699b466645f2452
artn-dev <azemarteixeira@gmail.com>
2021-08-09 15:50:24 -0300

    Lendo csv para data table

--- server.R
+++ server.R
@@ -1,3 +1,20 @@
 server <- function(input, output) {
+    dt_options <- list(
+        pageLength = 10,
+        language = list(
+            url = "//cdn.datatables.net/plug-ins/1.10.11/i18n/Portuguese-Brasil.json"
+        )
+    )
 
+    dt_data <- eventReactive(input$dt_select, {
+        dt_data <- read.csv("SteamCharts.csv")
+        dt <- subset(dt_data, dt_data$gamename == input$game_select)
+
+        return(dt)
+    })
+
+    output$charts <- renderDataTable(
+        dt_data() %>% as.data.frame(),
+        options = dt_options
+    )
 }
--- ui.R
+++ ui.R
@@ -1,7 +1,35 @@
+data_config <- box(
+    width = 12,
+    status = "warning",
+    solidHeader = TRUE,
+    title = "Configure Dataset",
+    selectInput(
+        "game_select",
+        "Selecione o Jogo",
+        c(
+            "Counter-Strike: Global Offensive",
+            "Dota 2",
+            "PLAYERUNKOWN'S BATTLEGROUNDS",
+            "Apex Legends",
+            "Rust"
+        )
+    ),
+    actionButton("dt_select", "Submeter")
+)
+
+data_table <- box(
+    width = 12,
+    dataTableOutput("charts")
+)
+
+
 header <- dashboardHeader()
 
 sidebar <- dashboardSidebar()
 
-body <- dashboardBody()
+body <- dashboardBody(
+    data_config %>% fluidRow(),
+    data_table  %>% fluidRow()
+)
 
 ui <- dashboardPage(header, sidebar, body)
